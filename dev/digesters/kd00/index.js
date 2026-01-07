565b1340d8b045dca20858817078ab7e6067c369
Jonathan Pool <pool@jpdev.pro>
2026-01-07 09:27:57 -0800

    convert digest issues to details and summaries

--- dev/digesters/kd00/index.js
+++ dev/digesters/kd00/index.js
@@ -26,12 +26,15 @@ const populateQuery = async (report, query) => {
   const {score} = report;
   const {details} = score;
   const {element, issue} = details;
+  // Initialize an array of data on the reported issues.
   const issueData = [];
+  // For each issue:
   Object.keys(issue).forEach(issueID => {
     const {summary, tools, wcag} = issue[issueID];
     const issueToolNames = Object.keys(tools).map(toolID => toolNames[toolID]);
     const issueXPaths = element[issueID];
     const elementCount = issueXPaths.length;
+    // Add data on it to the array.
     issueData.push({
       summary,
       wcag,
@@ -40,17 +43,23 @@ const populateQuery = async (report, query) => {
       elementCount
     });
   });
+  // Sort the array in order of decreasing count of reporting tools.
   issueData.sort((a, b) => b.issueToolNames.length - a.issueToolNames.length);
+  // Initialize the HTML lines rendering the facts about the issues.
   const dataLines = [];
+  // For each issue:
   issueData.forEach(issueDatum => {
     const {elementCount, issueToolNames, summary, wcag} = issueDatum;
-    dataLines.push(`<h3>${summary}</h3>`);
-    dataLines.push(`<p>Why it matters: ${issueDatum.why}</p>`);
+    // Add a details element with the issue summary as a summary element to the lines.
+    dataLines.push('<details>');
+    dataLines.push(`  <summary><h3>${summary}</h3></summary>`);
+    dataLines.push(`  <p>Why it matters: ${issueDatum.why}</p>`);
     if (wcag) {
-      dataLines.push(`<p>Related WCAG standard: ${wcag}</p>`);
+      dataLines.push(`  <p>Related WCAG standard: ${wcag}</p>`);
     }
-    dataLines.push(`<p>Reported by: ${issueToolNames.join(', ')}</p>`);
-    dataLines.push(`<p>Elements identified: ${elementCount}</p>`);
+    dataLines.push(`  <p>Reported by: ${issueToolNames.join(', ')}</p>`);
+    dataLines.push(`  <p>Elements identified: ${elementCount}</p>`);
+    dataLines.push('</details>');
   });
   query.data = dataLines.join(outerJoiner);
 };

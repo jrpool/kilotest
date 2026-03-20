3f97a20b6297ecfd5f89512e288f17a2d3943c90
Jonathan Pool <pool@jpdev.pro>
2026-03-20 08:29:01 -0700

    start adding test ordering

---
+++ testRec/index.js
@@ -0,0 +1,49 @@
+/*
+  index.js
+  Answers the retest question.
+*/
+
+// IMPORTS
+
+const {getJSON, getLog, getNowStamp, getPlainText} = require('../util');
+const fs = require('fs/promises');
+const path = require('path');
+
+// FUNCTIONS
+
+// Records a retest recommendation and returns an acknowledgement page.
+exports.answer = async (pageArgs, why) => {
+  const [timeStamp, jobID] = pageArgs.split('/');
+  const log = await getLog(timeStamp, jobID);
+  const targetWhat = log.pageWhat;
+  const recsPath = path.join(__dirname, '..', 'jobs', 'recs.json');
+  // Get the data on waiting recommendations.
+  const recsJSON = await fs.readFile(recsPath, 'utf8');
+  const recs = JSON.parse(recsJSON);
+  recs[targetWhat] ??= [];
+  const plainWhy = getPlainText(why);
+  // Add the recommendation to those for the target.
+  recs[targetWhat].push({
+    timeStamp: getNowStamp(),
+    why: plainWhy
+  });
+  // Save the revised recommendations.
+  await fs.writeFile(recsPath, getJSON(recs));
+  const query = {
+    target: targetWhat,
+    why: plainWhy
+  };
+  // Log the recommendation.
+  console.log(`Retest recommendation received for ${targetWhat}: ${plainWhy}`);
+  // Get the template.
+  let answerPage = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
+  // Replace its placeholders.
+  Object.keys(query).forEach(param => {
+    answerPage = answerPage.replace(new RegExp(`__${param}__`, 'g'), query[param]);
+  });
+  // Return the populated page.
+  return {
+    status: 'ok',
+    answerPage
+  };
+};

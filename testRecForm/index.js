6db76e4e76186ed922985eff4871d2183d38076a
Jonathan Pool <pool@jpdev.pro>
2026-03-21 10:01:25 -0700

    debug

--- testRecForm/index.js
+++ testRecForm/index.js
@@ -5,30 +5,16 @@
 
 // IMPORTS
 
-const {getAgoString, getDateTimeString, getLog} = require('../util');
 const fs = require('fs/promises');
 const path = require('path');
 
 // FUNCTIONS
 
-// Returns a retest recommendation form.
-exports.answer = async pageArgs => {
-  const [timeStamp, jobID] = pageArgs.split('/');
-  const log = await getLog(timeStamp, jobID);
-  const query = {
-    target: log.what,
-    timeStamp,
-    jobID,
-    ago: getAgoString(timeStamp),
-    dateTime: getDateTimeString(timeStamp)
-  };
+// Returns a test recommendation form.
+exports.answer = async () => {
   // Get the recommendation form template.
   let answerPage = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
-  // Replace its placeholders.
-  Object.keys(query).forEach(param => {
-    answerPage = answerPage.replace(new RegExp(`__${param}__`, 'g'), query[param]);
-  });
-  // Return the populated page.
+  // Return it.
   return {
     status: 'ok',
     answerPage

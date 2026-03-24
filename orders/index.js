0e5c6e64b3ed2c9d2cd947fb76593f012b003327
Jonathan Pool <pool@jpdev.pro>
2026-03-24 14:42:39 -0700

    add no-queue message

--- orders/index.js
+++ orders/index.js
@@ -33,6 +33,8 @@ const populateQuery = async query => {
     // Add the lines to the query.
     query[category] = lines[category].join('\n');
   }
+  // Add a no-queued message, if applicable, to the query.
+  query.noQueued = lines.queue.length ? '' : 'No pages are queued for testing.';
   // Add a no-claimed message, if applicable, to the query.
   query.noClaimed = lines.claimed.length ? '' : 'No pages are being tested now.';
 };

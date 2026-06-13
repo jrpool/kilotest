b1a978329be51c39b67210690db323d7c2c9daa8
Jonathan Pool <pool@jpdev.pro>
2026-06-13 08:01:10 -0700

    debug

--- researchAgent.js
+++ researchAgent.js
@@ -40,11 +40,15 @@ const requestService = async () => {
   const requestOptions = {
     method: 'POST',
     headers: {
-      host: kilotestHost,
-      pathname: `/${service}/${log.jobName.split('-').join('/')}`,
+      host: kilotestHost.split(':')[1].slice(2),
+      pathname: `${service}/${log.jobName.split('-').join('/')}`,
       'content-type': 'application/json; charset=utf-8'
     }
   };
+  const clientType = client === httpsClient ? 'HTTPS' : 'HTTP';
+  console.log(
+    `About to submit ${clientType} request to ${kilotestHost}/${requestOptions.headers.pathname}`
+  );
   // Submit a request.
   client.request(requestOptions, response => {
     // Initialize a collection of data from the response.

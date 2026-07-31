5151bbc004e78d317a1e0f84daa6176e3749cce4
Jonathan Pool <pool@jpdev.pro>
2026-07-30 22:29:25 -0700

    continue refactoring

--- api/listViolators.js
+++ api/listViolators.js
@@ -5,21 +5,8 @@
 
 // IMPORTS
 
-const {
-  getIssueBasics,
-  getReportBasics,
-  getResponseMetadata,
-  getToolsFacts,
-  getViolatorBasics
-} = require('./util');
-const {
-  getDateTime,
-  getNowStamp,
-  getRandomString,
-  getReport,
-  isHidden,
-  tools
-} = require('../util');
+const {getReportBasics, getResponseMetadata, getToolsFacts} = require('./util');
+const {getReport, ruleEngines} = require('../util');
 const issuesClassification = require('testilo/procs/score/tic').issues;
 
 // CONSTANTS
@@ -28,39 +15,6 @@ const thisHost = process.env.THIS_KILOTEST_HOST;
 
 // FUNCTIONS
 
-// Returns the details about an issue in a report, not including a list of its violators.
-exports.getIssueDetails = (issueID, report) => {
-  // Initialize data about the issue.
-  const reporterIDs = new Set();
-  const violatorIndexes = new Set();
-  // For each act in the report:
-  report.acts.forEach(act => {
-    // If the act is a test act:
-    if (act.type === 'test') {
-      const {result, which} = act;
-      const instances = result?.standardResult?.instances ?? [];
-      // For each of the standard instances of the act:
-      instances.forEach(instance => {
-        // If the instance has the issue:
-        if (instance.issueID === issueID) {
-          // Ensure the rule engine is a reporter in the data.
-          reporterIDs.add(which);
-          const {catalogIndex} = instance;
-          // If the instance has a catalog index:
-          if (catalogIndex) {
-            // Ensure it is a violator in the data.
-            violatorIndexes.add(catalogIndex);
-          }
-        }
-      });
-    }
-  });
-  // Return the details.
-  return {
-    'names of rule engines with violated rules belonging to the issue': Array.from(reporterIDs),
-    'count of violators, i.e. elements exhibiting the issue': violatorIndexes.size
-  };
-};
 // Returns the response body.
 exports.response = async args => {
   const [issueID, timeStamp, jobID] = args;
@@ -70,6 +24,7 @@ exports.response = async args => {
   const responseContent = {
     'basics about the report': null,
     'details about the issue': null,
+    'rule engines reporting violations belonging to the issue': null,
     'basics about the violators of rules belonging to the issue': null
   };
   // Get the report.
@@ -85,17 +40,29 @@ exports.response = async args => {
     responseContent['basics about the report'] = await getReportBasics(timeStamp, jobID);
     // Get the issue classification.
     const issueClassification = issuesClassification[issueID];
-    const {summary, weight, why} = issueClassification;
-    // If the instance has a non-ignorable and fully classified issue:
+    const {summary, wcag, weight, why} = issueClassification;
+    // If the issue is non-ignorable and fully classified:
     if (
       issueID
       && issueID !== 'ignorable'
       && issueClassification
+      && summary
       && [1, 2, 3, 4].includes(weight)
       && why
     ) {
+      // Add the details about the issue to the response content.
+      responseContent['details about the issue'] = {
+        'identifier': issueID,
+        summary,
+        'impact on a user': why,
+        'related WCAG standard': {
+          'layer': (wcag?.length > 4 ? 'success criterion' : 'guideline') || null,
+          'identifier': wcag || null
+        },
+        priority: ['lowest', 'low', 'high', 'highest'][weight - 1]
+      };
       // Initialize data about the instances of the issue.
-      const ruleEngineIDs = new Set();
+      const reporterIDs = new Set();
       const violatorIndexes = new Set();
       // For each act in the report:
       report.acts.forEach(act => {
@@ -109,7 +76,7 @@ exports.response = async args => {
             // If the instance has the issue ID:
             if (instance.issueID === issueID) {
               // Ensure the rule-engine ID is in the data.
-              ruleEngineIDs.add(which);
+              reporterIDs.add(which);
               // If the instance has a catalog index:
               if (catalogIndex) {
                 // Ensure the catalog index is in the data.
@@ -119,92 +86,67 @@ exports.response = async args => {
           });
         }
       });
+      // Add the details about the reporters of the issue to the response content.
+      responseContent['rule engines reporting violations belonging to the issue'] = Array
+      .from(reporterIDs)
+      .map(reporterID => {
+        const reporter = ruleEngines[reporterID];
+        return {
+          identifier: reporterID,
+          name: reporter?.[0] || null,
+          description: reporter?.[1] || null
+        };
+      });
       // Initialize the basics about the violators.
       const violatorsBasics = [];
       // Get the violator catalog indexes, sorted by DOM order.
       const sortedViolatorIndexes = Array
       .from(violatorIndexes)
       .sort((a, b) => Number(a) - Number(b));
-      // For each violator:
+      // For each violator index:
       sortedViolatorIndexes.forEach(violatorIndex => {
         const catalogItem = report.catalog[violatorIndex];
-        // Get the basics about it.
-        const violatorBasics = {
-          'index in the DOM': Number(violatorIndex),
-          ''
+        // If it is the index of a catalog item:
+        if (catalogItem) {
+          const {tagName, text} = catalogItem;
+          // Get the basics about it.
+          const violatorBasics = {
+            'index in the DOM': Number(violatorIndex),
+            'tag name': tagName || null,
+            'inner text': text || null,
+          };
+          // Add them to the basics about the violators.
+          violatorsBasics.push(violatorBasics);
         }
-        // Add the catalog index to the basics about the violators.
-        violatorsBasics.push({
-          catalogIndex: violatorIndex
-        });
       });
-      // Add the catalog indexes of the violators of rules belonging to the issue to the data.
-      responseContent['basics about the violators of rules belonging to the issue'] = {
-        issueID,
-        summary,
-        weight,
-        why,
-        violatorIndexes: Array.from(violatorIndexes)
-      };
+      // Add the basics about the violators to the response content.
+      responseContent['basics about the violators of rules belonging to the issue']
+      = violatorsBasics;
     }
-
-
-  const thisHost = process.env.THIS_KILOTEST_HOST;
-  // Get a response.
-  const content = {
-    summary: `This document fulfills a request made by a language model to a Kilotest tool. The model requested data, drawn from a Kilotest report, about one of the issues for the front-end quality (i.e. accessibility, usability, and standards conformity) of a web page. Kilotest, with the help of Testaro, Testilo, and an ensemble of ten rule engines, performs tests on web pages, using a combination of rule- and machine-learning-based methods, and produces reports. Kilotest exposes several API endpoints to recommend web pages for testing and to obtain information from Kilotest reports. To learn more about Kilotest and the advantages of testing with an ensemble of rule engines, visit the deployed instance of Kilotest (${process.env.DEPLOYED_KILOTEST_HOST}), which contains an introduction on its home page and a tutorial.`,
-    'tool collection name': 'Kilotest',
-    'tool name': 'describeOneIssueFromOneReport',
+  }
+  // Create a response body.
+  const body = {
+    'tool collection': toolsFacts,
+    'tool name': 'listViolators',
     request: {
-      'type of request': {
-        identifier: 'reportIssue',
-        description: 'Describe one issue from one report.'
-      },
+      description: 'Provide details about one issue in one report, including basics about the elements of the tested page that were reported as exhibiting the issue. The issueID, timeStamp, and jobID parameters identify the issue and report that I want details about. Those parameters were in the response to my earlier listIssues request.',
       method: 'GET',
       URLs: {
-        'for JSON output': `${thisHost}/api/reportIssue/${issueID}/${timeStamp}/${jobID}`,
-        'for HTML output': `${thisHost}/reportIssue.html/${issueID}/${timeStamp}/${jobID}`
+        'for JSON output': `${thisHost}/api/listIssues/${issueID}/${timeStamp}/${jobID}`,
+        'for HTML output': `${thisHost}/reportIssues.html/${issueID}/${timeStamp}/${jobID}`
       },
       'closest ancestor request': {
-        identifier: 'summarizeOneReport',
-        description: 'Summarize one report.',
+        'tool name': 'listIssues',
+        description: 'Provide details about one report, including basics about the issues reported in it. The timeStamp and jobID parameters identify the report that I want details about.',
         URLs: {
-          'for JSON output': `${thisHost}/api/reportFacts/${timeStamp}/${jobID}`,
+          'for JSON output': `${thisHost}/api/listIssues/${timeStamp}/${jobID}`,
           'for HTML output': `${thisHost}/reportIssues.html/${timeStamp}/${jobID}`
         }
       }
     },
-    'response metadata': {
-      identifier: `${getNowStamp()}-${getRandomString(3)}`,
-      'date and time': new Date().toISOString()
-    },
-    report: {
-      identifier: `${timeStamp}-${jobID}`,
-      'creation date': getDateTime(timeStamp),
-      'days since the creation date': daysAgo
-    },
-    'tested web page': {
-      description: what,
-      URL: url
-    },
-    'rule engines that tried to test the page': getToolsFacts(Object.keys(tools)),
-    'rule engines that were unable to test the page': preventedTools,
-    'rule engines that reported issues': {
-      number: reporterCount,
-      names: reporters.map(tool => tool.toolName)
-    },
-    'number of issues reported': {
-      total: issueCount,
-      'by priority': {
-        'highest priority': issues[4].length,
-        'high priority': issues[3].length,
-        'low priority': issues[2].length,
-        'lowest priority': issues[1].length
-      }
-    },
-    'number of HTML elements reported as exhibiting issues': violatorCount,
-    'level of the issue': issueLevel,
-    'facts about the issue': await getIssueFacts(issue, timeStamp, jobID)
-  };
-  return content;
+    'response metadata': getResponseMetadata(),
+    'response content': responseContent
+  }
+  // Return it.
+  return body;
 };

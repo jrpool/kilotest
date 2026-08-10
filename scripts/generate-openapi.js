40368be99baf089a339950422126135382eacdcc
Jonathan Pool <pool@jpdev.pro>
2026-08-10 16:28:00 -0700

    debug

--- scripts/generate-openapi.js
+++ scripts/generate-openapi.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 const {createDocument} = require('zod-openapi');
-const {version} = require('../package.json');
+const {license, version} = require('../package.json');
 const routes = require('../api/routes');
 
 // CONSTANTS
@@ -58,6 +58,9 @@ const generate = () => {
       title: 'Kilotest API',
       description: 'Kilotest is an ensemble testing and reporting service for front-end web quality. It uses an ensemble of 10 rule engines to test public web pages for front-end quality (accessibility, usability, and standards conformity). Kilotest acts as a web server for human users, an MCP server for AI platforms, and an API for programmatic access. With this API you can request that a page be tested or retested, discover available reports, incrementally retrieve facts from a report: from what reports are available, to what issues were reported in one report, to what elements violated one issue, to what diagnoses rule engines gave for one violation.',
       version,
+      license: {
+        name: license
+      },
       contact: {
         name: 'Kilotest',
         url: 'https://github.com/jrpool/kilotest',
